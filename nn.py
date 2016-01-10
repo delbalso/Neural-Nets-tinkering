fa99998d6e24d7ed26853642187f342b16b9e639
Mike Del Balso <mdb@uber.com>
2016-01-09 17:28:39 -0800

    moved weights into NN object, added python plots

--- nn.py
+++ nn.py
@@ -3,8 +3,31 @@ import pandas as pd
 import numpy as np
 import random
 import mnist_loader as mnist
+import matplotlib.pyplot as plt
+
 
 np.random.seed(12345678)
+training_accuracy_history = list()
+test_accuracy_history = list()
+
+
+def plot(training_accuracy_history):
+    font = {'family': 'sans-serif',
+            'color': 'black',
+            'weight': 'normal',
+            'size': 16,
+            }
+
+    plt.plot(training_accuracy_history, 'b-', label="Training Data")
+    plt.plot(test_accuracy_history, 'r-', label="Test Data")
+    plt.title('Model accuracy during training', fontdict=font)
+    plt.xlabel('Training Epochs', fontdict=font)
+    plt.ylabel('Classification Accuracy (%)', fontdict=font)
+    plt.legend(loc='lower right')
+
+# Tweak spacing to prevent clipping of ylabel
+    plt.subplots_adjust(left=0.15)
+    plt.show()
 
 
 def logistic(i):
@@ -25,45 +48,69 @@ def accuracy(output, labels):
     output_dist = list()
     label_dist = list()
     label_values = np.argmax(labels, axis=0)
-    for i in xrange(10):
-        output_dist.append((output_values == i).sum())
-        label_dist.append((label_values == i).sum())
-    print output_dist
-    print label_dist
+    if False:  # Use this to print distributions
+        for i in xrange(10):
+            output_dist.append((output_values == i).sum())
+            label_dist.append((label_values == i).sum())
+        print output_dist
+        print label_dist
     return sum(output_values == label_values) / float(len(label_values))
 
 # input layer = a0
 # input -> a1 = w1 * a0 -> a2 = w2 * a1
 
 
-def train(labels, features, nn, epochs, mini_batch_size):
-    # initalize
-    num_training_data = labels[1, :].size
-    a = dict()
-# set up inital weights
+def initialize_weights(nn):
     w = dict()
-    delta_w = dict()
-    delta = dict()
     for i in xrange(1, nn.index_of_final_layer + 1):
         limit = np.sqrt(
             float(6) / (nn.layers[i - 1].size + 1 + nn.layers[i].size))
         w[i] = np.random.uniform(-1 * limit, limit,
                                  (nn.layers[i].size, nn.layers[i - 1].size + 1))
+    nn.w = w
+
+
+def train(
+        labels,
+        features,
+        nn,
+        epochs,
+        mini_batch_size,
+        test_labels,
+        test_features):
+    # initalize
+    num_training_data = labels[1, :].size
+    a = dict()
+# set up inital weights
+    delta_w = dict()
+    delta = dict()
+    initialize_weights(nn)
 
     for j in range(epochs):  # number of training loops
         print "Starting epoch " + str(j) + " of " + str(epochs)
+# Calculate test/training accuracy for plotting
+        training_accuracy = accuracy(
+            feedforward(
+                features, nn)[
+                nn.index_of_final_layer], labels)
+        test_accuracy = accuracy(
+            feedforward(
+                test_features, nn)[
+                nn.index_of_final_layer], test_labels)
+        training_accuracy_history.append(training_accuracy)
+        test_accuracy_history.append(test_accuracy)
+        print "training accuracy: " + str(training_accuracy)
+        if training_accuracy == 1:
+            break
 
-        for mini_batch_index in xrange(0,num_training_data,mini_batch_size):
-            features_batch = features[:,mini_batch_index:mini_batch_index+mini_batch_size]
-            labels_batch = labels[:,mini_batch_index:mini_batch_index+mini_batch_size]
-            this_batch_size = labels_batch[1,:].size
-            a = feedforward(features_batch, w, nn)
-            if j % 10 == 0 and mini_batch_index==0:
-                training_accuracy = accuracy(a[nn.index_of_final_layer], labels_batch)
-                if training_accuracy == 1:
-                    break
-                print "training accuracy: " + str(training_accuracy)
-            for l in sorted(w.keys(), reverse=True):
+        for mini_batch_index in xrange(0, num_training_data, mini_batch_size):
+            features_batch = features[
+                :, mini_batch_index:mini_batch_index + mini_batch_size]
+            labels_batch = labels[
+                :, mini_batch_index:mini_batch_index + mini_batch_size]
+            this_batch_size = labels_batch[1, :].size
+            a = feedforward(features_batch, nn)
+            for l in sorted(nn.w.keys(), reverse=True):
                 # compute error, elementwise computations
                 sigma_derivative = np.multiply(a[l], 1 - a[l])  # 10 x 1593
 
@@ -75,14 +122,22 @@ def train(labels, features, nn, epochs, mini_batch_size):
 
                 elif l == nn.index_of_final_layer - 1:
                     delta[l] = np.multiply(
-                        np.dot(w[l + 1].transpose(), delta[l + 1]), sigma_derivative)  # 10 x 1593
+                        np.dot(
+                            nn.w[
+                                l + 1].transpose(),
+                            delta[
+                                l + 1]),
+                        sigma_derivative)  # 10 x 1593
                 else:
                     delta[l] = np.multiply(
-                        np.dot(w[l + 1].transpose(), delta[l + 1][1:]), sigma_derivative)  # 10 x 1593
+                        np.dot(nn.w[l + 1].transpose(), delta[l + 1][1:]), sigma_derivative)  # 10 x 1593
 
              # compute delta W
                 weight_deltas = np.zeros(
-                    (this_batch_size, nn.layers[l].size, nn.layers[l - 1].size + 1))
+                    (this_batch_size,
+                     nn.layers[l].size,
+                        nn.layers[
+                         l - 1].size + 1))
                 learning_rate = .15
                 l2_param = 0.1
                 for datum in range(this_batch_size):
@@ -96,18 +151,17 @@ def train(labels, features, nn, epochs, mini_batch_size):
 # Apply regularization
                 regularization = 1 - learning_rate * l2_param / this_batch_size
                 regularization_mat = np.append(
-                    [1], np.ones(w[l].shape[1] - 1) * regularization)
-                w[l] = w[l] * regularization_mat[None, :] - \
+                    [1], np.ones(nn.w[l].shape[1] - 1) * regularization)
+                nn.w[l] = nn.w[l] * regularization_mat[None, :] - \
                     learning_rate * delta_w[l]
-    return w
 
 
-def feedforward(features, w, nn):
+def feedforward(features, nn):
     a = dict()
     a[0] = np.ones((features.shape[0] + 1, features.shape[1]))
     a[0][1:, :] = features
-    for l in w:
-        z = np.dot(w[l], a[l - 1])
+    for l in nn.w:
+        z = np.dot(nn.w[l], a[l - 1])
         if (nn.layers[l].ltype == Layer.T_LOGISTIC):
             no_bias_a = logistic(z)
         elif (nn.layers[l].ltype == Layer.T_SOFTMAX):
@@ -122,8 +176,8 @@ def feedforward(features, w, nn):
     return a
 
 
-def test(labels, features, weights, nn):
-    a = feedforward(features, weights, nn)
+def test(labels, features, nn):
+    a = feedforward(features, nn)
     output = a[nn.index_of_final_layer]  # 10 x 1593
     # print output
     test_accuracy = accuracy(output, labels)
@@ -135,6 +189,7 @@ class NN():
     def __init__(self, layers):
         self.layers = layers
         self.index_of_final_layer = len(layers) - 1
+        self.w = dict()
 
 
 class Layer():
@@ -175,15 +230,25 @@ def main():
     """
     training_data, validation_data, test_data = mnist.load_data_wrapper_1()
     random.shuffle(training_data)
-    training_features, training_labels = zip(*training_data[:10000])
-    print np.array(training_features).shape
+# train
+    training_features, training_labels = zip(*training_data[:1000])
     training_features = np.squeeze(training_features).transpose()
     training_labels = np.squeeze(training_labels).transpose()
-    weights = train(training_labels, training_features,nn,30,30)
+# test
     test_features, test_labels = zip(*test_data)
     test_features = np.squeeze(test_features).transpose()
     test_labels = np.squeeze(test_labels).transpose()
-    test(test_labels,test_features,weights,nn)
+
+    train(
+        training_labels,
+        training_features,
+        nn,
+        10,
+        30,
+        test_labels,
+        test_features)
+    plot(training_accuracy_history)
+    test(test_labels, test_features, nn)
 
 
 if __name__ == "__main__":
